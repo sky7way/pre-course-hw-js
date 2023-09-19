0cd3b49accceb695378f5bfe00b6f49e3cba4709
Aleksandr <sky7way@yandex.ru>
2023-09-20 01:26:52 +0300

    2-6

---
+++ lesson-02/06.js
@@ -0,0 +1,16 @@
+// code
+
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddress1 = JSON.parse(JSON.stringify(passportWithAddress));
+passportWithAddress1["address"]["city"] = 'Bobryisk';
+
+console.log(passportWithAddress["address"]["city"]);
+console.log(passportWithAddress1["address"]["city"]);
