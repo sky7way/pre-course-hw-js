dc4f65b89ade62e266ae246bd3a8f60b55ef85bd
Aleksandr <sky7way@yandex.ru>
2023-09-20 00:59:21 +0300

    2-2

---
+++ lesson-02/02.js
@@ -0,0 +1,11 @@
+// code
+
+function factorial(n) {
+    debugger
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(5));
