01708c18e5aa15537e8eaad8ec8b6a7911434379
Aleksandr <sky7way@yandex.ru>
2023-09-20 01:45:06 +0300

    2-6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -9,8 +9,16 @@ let passportWithAddress = {
     }
 };
 
-let passportWithAddress1 = JSON.parse(JSON.stringify(passportWithAddress));
-passportWithAddress1["address"]["city"] = "Bobryisk";
+let copiedPassportWithAddress = JSON.parse(JSON.stringify(passportWithAddress));
 
-console.log(passportWithAddress.address.city);
-console.log(passportWithAddress1.address.city);
+copiedPassportWithAddress.address.city = "Bobryisk";
+
+console.log("Исходный объект:", passportWithAddress);
+console.log("Скопированный объект с измененным city:", copiedPassportWithAddress);
+
+
+// let passportWithAddress1 = JSON.parse(JSON.stringify(passportWithAddress));
+// passportWithAddress1["address"]["city"] = "Bobryisk";
+//
+// console.log(passportWithAddress.address.city);
+// console.log(passportWithAddress1.address.city);
