eb516fd8e6082c8c9fb84d0ce7b7b5808d3c56fb
Aleksandr <sky7way@yandex.ru>
2023-09-20 00:08:57 +0300

    1-2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -2,10 +2,10 @@
 
 let number1 = Math.floor(Math.random() * 100);
 const isEvenNumber = (num) => {
-    if (num % 2) {
-        console.log(`${num}: Это нечетное число`);
-    } else {
+    if (num % 2 === 0) {
         console.log(`${num}: Это четное число`);
+    } else {
+        console.log(`${num}: Это нечетное число`);
     }
 }
 
