7003017729cc1e7adeb0718c1f0bc8a4047cb10c
Aleksandr <sky7way@yandex.ru>
2023-09-20 01:37:25 +0300

    2-6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -10,8 +10,7 @@ let passportWithAddress = {
 };
 
 let passportWithAddress1 = JSON.parse(JSON.stringify(passportWithAddress));
-passportWithAddress1["address"]["city"] = 'Bobryisk';
+passportWithAddress1["address"]["city"] = "Bobryisk";
 
-console.log(passportWithAddress["address"]["city"]);
-console.log(passportWithAddress1["address"]["city"]);
-console.log()
+console.log(passportWithAddress["address"]);
+console.log(passportWithAddress1["address"]);
