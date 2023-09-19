48710a8e37021d4fc41a3a7ec16996c0fdd9219f
Aleksandr <sky7way@yandex.ru>
2023-09-20 00:04:53 +0300

    1-2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,12 @@
 // code
+
+let number1 = Math.floor(Math.random() * 100);
+const isEvenNumber = (num) => {
+    if (num % 2) {
+        console.log(`${num}: Это нечетное число`);
+    } else {
+        console.log(`${num}: Это четное число`);
+    }
+}
+
+isEvenNumber(number1);
