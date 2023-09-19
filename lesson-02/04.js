88f9eb6840ce1d54aa97e0e69584f24601925e6f
Aleksandr <sky7way@yandex.ru>
2023-09-20 01:07:22 +0300

    2-4

---
+++ lesson-02/04.js
@@ -0,0 +1,11 @@
+// code
+const giveMeRightCondition = (value1, value2, maximum) => {
+    if (value1 === value2 || value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+
+giveMeRightCondition(500, 100, 200)
+
